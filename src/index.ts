aa58917bac247669b2db1aded430c141925f5f6a
Kumar <ankit.ay.kumar@capgemini.com>
2024-04-24 14:38:58 +0530

    cmt

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import app from './app';
 import {testFun} from './libraries/aes.algo.library'
-const asd = testFun(1,2)
-console.log(asd)
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
